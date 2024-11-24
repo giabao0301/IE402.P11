9c3a75533cb614014f47d06f15e91e052a879a1f
giabao0301 <21521866@gm.uit.edu.vn>
2024-11-24 21:47:55 +0700

    first commit

--- api/app.js
+++ api/app.js
@@ -21,6 +21,6 @@ app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(8800, () => {
+app.listen(8080, () => {
   console.log("Server is running!");
 });
